9f576c46a2b055b06fe6924bf8905dd93389d1a6
janemir <janemiro2005@gmail.com>
2024-12-15 01:35:08 +0300

    added some styles to App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,6 +5,8 @@ import { Label } from "@/components/ui/label";
 import Login from "@/login"; // Импорт страницы входа
 import Post from "@/post"; // Импорт страницы постов
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import "./Home.css";
+
 
 function App() {
     return (
@@ -27,26 +29,29 @@ function Home() {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <Label htmlFor="create_account">Создать аккаунт</Label>
-            <Label htmlFor="email">Почта</Label>
-            <Input id="email" placeholder="Введите почту" />
-            <Label htmlFor="password">Пароль</Label>
-            <Input id="password" placeholder="Введите пароль" />
-            <Label htmlFor="repeat_password">Повторите пароль</Label>
-            <Input id="repeat_password" placeholder="Повторите пароль" />
-            <Label htmlFor="select_role">Выберите роль</Label>
-            <Tabs defaultValue="account" className="w-[400px]">
-                <TabsList>
-                    <TabsTrigger value="reader">Читатель</TabsTrigger>
-                    <TabsTrigger value="author">Автор</TabsTrigger>
-                </TabsList>
-            </Tabs>
-            <Button onClick={() => navigate("/post")}>Создать аккаунт</Button>
-            <Label>Уже есть аккаунт?</Label>
-            <Link to="/login" style={{ color: "#6366F1" }}>Войти</Link>
-        </div>
+
+            <div className="form-container">
+                <Label htmlFor="create_account">Создать аккаунт</Label>
+                <Label htmlFor="email">Почта</Label>
+                <Input id="email" placeholder="Введите почту" />
+                <Label htmlFor="password">Пароль</Label>
+                <Input id="password" placeholder="Введите пароль" />
+                <Label htmlFor="repeat_password">Повторите пароль</Label>
+                <Input id="repeat_password" placeholder="Повторите пароль" />
+                <Label htmlFor="select_role">Выберите роль</Label>
+                <Tabs defaultValue="account" className="w-[400px]">
+                    <TabsList>
+                        <TabsTrigger value="reader">Читатель</TabsTrigger>
+                        <TabsTrigger value="author">Автор</TabsTrigger>
+                    </TabsList>
+                </Tabs>
+                <Button onClick={() => navigate("/post")}>Создать аккаунт</Button>
+                <Label>Уже есть аккаунт?</Label>
+                <Link to="/login" style={{ color: "#6366F1" }}>Войти</Link>
+            </div>
+
     );
 }
+
 
 export default App;
