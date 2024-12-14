0152170ea89a86574c45af1c401e24af05cac846
janemir <janemiro2005@gmail.com>
2024-12-14 22:22:30 +0300

    test commit

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+
 
 
 function App() {
@@ -11,8 +13,16 @@ function App() {
 
   return (
       <div>
-          <Label htmlFor="email">Your email address</Label>
+          <Label htmlFor="create_account">Создать аккаунт</Label>
+          <Label htmlFor="email">Почта</Label>
+          <Input />
+          <Label htmlFor="password">Пароль</Label>
+          <Input />
+          <Label htmlFor="repeat_password">Повторите пароль</Label>
+          <Input />
           <Button>Создать аккаунт</Button>
+          <Label htmlFor="repeat_password">Уже есть аккаунт?</Label>
+          <Label htmlFor="repeat_password">Войти</Label>
       </div>
   )
 }
