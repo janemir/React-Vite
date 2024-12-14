35f69c832026a56493035ded888e59b0aca667f4
janemir <janemiro2005@gmail.com>
2024-12-15 00:51:27 +0300

    add tabs

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import Login from "@/login"; // Импорт страницы входа
 import Post from "@/post"; // Импорт страницы постов
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 function App() {
     return (
@@ -34,6 +35,13 @@ function Home() {
             <Input id="password" placeholder="Введите пароль" />
             <Label htmlFor="repeat_password">Повторите пароль</Label>
             <Input id="repeat_password" placeholder="Повторите пароль" />
+            <Label htmlFor="select_role">Выберите роль</Label>
+            <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList>
+                    <TabsTrigger value="reader">Читатель</TabsTrigger>
+                    <TabsTrigger value="author">Автор</TabsTrigger>
+                </TabsList>
+            </Tabs>
             <Button onClick={() => navigate("/post")}>Создать аккаунт</Button>
             <Label>Уже есть аккаунт?</Label>
             <Link to="/login" style={{ color: "#6366F1" }}>Войти</Link>
--- vite-project/src/post.tsx
+++ vite-project/src/post.tsx
@@ -1,5 +1,31 @@
 function Post() {
-    return <div>Добро пожаловать на страницу постов!</div>;
+    return (
+            <header className="post-header">
+                <div className="post-header-left">
+                    <img
+                        src="/src/assets/logo.svg"
+                        alt="Левая картинка"
+                        className="left-image"
+                    />
+                </div>
+                <div className="post-header-right">
+                    <p>Добро пожаловать!</p>
+                    <img
+                        src="/src/assets/avatar.svg"
+                        alt="Правая картинка"
+                        className="right-image"
+                    />
+                </div>
+                <div className="post-picture">
+                    <img
+                        src="/src/assets/adv.png"
+                        alt="Правая картинка"
+                        className="picture"
+                    />
+                </div>
+            </header>
+
+    );
 }
 
 export default Post;
