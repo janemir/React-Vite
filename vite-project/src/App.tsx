31ad1c4b44a55a9f49d395f81ddd44fb6c185095
janemir <janemiro2005@gmail.com>
2024-12-14 21:18:43 +0300

    added label and button

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <div>
-        <Button>Click me</Button>
+          <Label htmlFor="email">Your email address</Label>
+          <Button>Создать аккаунт</Button>
       </div>
   )
 }
